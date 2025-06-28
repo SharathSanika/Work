a08dedbe1d8c6310fd7a902f383911116215a6c6
user <user@DESKTOP-DHSI56N.lan>
2025-06-27 23:37:31 -0700

    tc_002

--- src/test/java/works/Login.java
+++ src/test/java/works/Login.java
@@ -3,8 +3,8 @@ package works;
 public class Login {
 
 	public static void main(String[] args) {
-		System.out.println("helloSharath");
-		System.out.println("hellosss");
+		System.out.println("git class inprogress");
+		
 
 		
 	}
